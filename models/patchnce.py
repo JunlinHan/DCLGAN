724e7bf67e7cd33fbc086f19875db9a376a98caa
JunlinHan <48272064+JunlinHan@users.noreply.github.com>
2021-04-13 18:43:32 +1000

    Update patchnce.py

--- models/patchnce.py
+++ models/patchnce.py
@@ -2,9 +2,9 @@ from packaging import version
 import torch
 from torch import nn
 
-# Note this is different from the original CUT. We use cosine similarity here, but both implementations are identical ( after the L2 normalization).
-# You may replace this using the pathnece.py from CUT if you change the default setting.
-# But we recommend using this implementation, it usually gives slightly better performance.
+# Note this is different from the vanilla CUT. I use cosine similarity here, but both implementations are identical ( after the L2 normalization).
+# You may replace this with class PatchNCEloss2 ( used in vanilla CUT, no cosine similarity)
+# I recommend using this implementation, it usually gives slightly better performance ( though I haven't find the reason yet).
 class PatchNCELoss(nn.Module):
     def __init__(self, opt):
         super().__init__()
@@ -41,9 +41,56 @@ class PatchNCELoss(nn.Module):
         out = torch.cat((l_pos, l_neg), dim=1) / self.opt.nce_T
         loss = self.cross_entropy_loss(out, torch.zeros(out.size(0), dtype=torch.long,
                                                         device=feat_q.device))
-
         return loss
 
 
+# Used in vanilla CUT
+class PatchNCELoss2(nn.Module):
+    def __init__(self, opt):
+        super().__init__()
+        self.opt = opt
+        self.cross_entropy_loss = torch.nn.CrossEntropyLoss(reduction='none')
+        self.mask_dtype = torch.uint8 if version.parse(torch.__version__) < version.parse('1.2.0') else torch.bool
 
+    def forward(self, feat_q, feat_k):
+        batchSize = feat_q.shape[0]
+        dim = feat_q.shape[1]
+        feat_k = feat_k.detach()
+
+        # pos logit
+        l_pos = torch.bmm(feat_q.view(batchSize, 1, -1), feat_k.view(batchSize, -1, 1))
+        l_pos = l_pos.view(batchSize, 1)
+
+        # neg logit
+
+        # Should the negatives from the other samples of a minibatch be utilized?
+        # In CUT and FastCUT, we found that it's best to only include negatives
+        # from the same image. Therefore, we set
+        # --nce_includes_all_negatives_from_minibatch as False
+        # However, for single-image translation, the minibatch consists of
+        # crops from the "same" high-resolution image.
+        # Therefore, we will include the negatives from the entire minibatch.
+        if self.opt.nce_includes_all_negatives_from_minibatch:
+            # reshape features as if they are all negatives of minibatch of size 1.
+            batch_dim_for_bmm = 1
+        else:
+            batch_dim_for_bmm = self.opt.batch_size
+
+        # reshape features to batch size
+        feat_q = feat_q.view(batch_dim_for_bmm, -1, dim)
+        feat_k = feat_k.view(batch_dim_for_bmm, -1, dim)
+        npatches = feat_q.size(1)
+        l_neg_curbatch = torch.bmm(feat_q, feat_k.transpose(2, 1))
+
+        # diagonal entries are similarity between same features, and hence meaningless.
+        # just fill the diagonal with very small number, which is exp(-10) and almost zero
+        diagonal = torch.eye(npatches, device=feat_q.device, dtype=self.mask_dtype)[None, :, :]
+        l_neg_curbatch.masked_fill_(diagonal, -10.0)
+        l_neg = l_neg_curbatch.view(-1, npatches)
+
+        out = torch.cat((l_pos, l_neg), dim=1) / self.opt.nce_T
+
+        loss = self.cross_entropy_loss(out, torch.zeros(out.size(0), dtype=torch.long,
+                                                        device=feat_q.device))
+        return loss
 
