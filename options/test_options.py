60e0bd539abb87494c10464a094a531519f63044
JunlinHan <48272064+JunlinHan@users.noreply.github.com>
2021-04-19 16:40:13 +1000

    Update test_options.py

--- options/test_options.py
+++ options/test_options.py
@@ -13,7 +13,8 @@ class TestOptions(BaseOptions):
         parser.add_argument('--phase', type=str, default='test', help='train, val, test, etc')
         # Dropout and Batchnorm has different behavioir during training and test.
         parser.add_argument('--eval', action='store_true', help='use eval mode during test time.')
-        parser.add_argument('--num_test', type=int, default=100, help='how many test images to run')
+        # Set the default = 5000 to test the whole test set.
+        parser.add_argument('--num_test', type=int, default=5000, help='how many test images to run')
 
         # To avoid cropping, the load_size should be the same as crop_size
         parser.set_defaults(load_size=parser.get_default('crop_size'))
